451a124fa7110145b900fcf95795f42ee80b5056
Elena Castellanos <e.castellanos@meridian.internal>
2021-06-10 16:49:00 -0500

    PLAT-1444 scaffold audit-trail-service
    Port from PORTS.md. Local profile only until the audit-trail-service CAB entry is approved.

---
+++ services/audit-trail-service/src/main/java/com/meridian/platform/audittrail/AuditTrailServiceApplication.java
@@ -0,0 +1,12 @@
+package com.meridian.platform.audittrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditTrailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditTrailServiceApplication.class, args);
+    }
+}
