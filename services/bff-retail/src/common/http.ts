0c90ba01e9bfdf89ea732a507ec10dec5eaf1ab5
Deborah Okafor <d.okafor@meridian.internal>
2021-06-11 16:36:00 -0400

    PLAT-1446 bff-retail transactions paging

---
+++ services/bff-retail/src/common/http.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosInstance } from 'axios';
+import { correlation } from './correlation';
+import { config } from '../config';
+
+/** Every outbound call carries the correlation id; Splunk joins the hops on it. */
+export function upstream(baseURL: string): AxiosInstance {
+  const client = axios.create({ baseURL, timeout: config.upstreamTimeoutMs });
+  client.interceptors.request.use((req) => {
+    req.headers = req.headers ?? {};
+    req.headers['X-Correlation-Id'] = correlation.current();
+    // Same Keystone audience on both sides, so the customer's token is relayed rather than
+    // minting a service token per hop. Keystone wanted per-service audiences in 2022 (KEY-3110)
+    // and backed off; if that ever lands this needs an exchange step.
+    const token = correlation.bearerToken();
+    if (token && !req.headers['Authorization']) {
+      req.headers['Authorization'] = `Bearer ${token}`;
+    }
+    req.headers['X-Channel'] = 'RTL';
+    return req;
+  });
+  return client;
+}
