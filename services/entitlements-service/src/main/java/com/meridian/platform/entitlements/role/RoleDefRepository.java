56172ce489ece6eb1de425e21722f9d6fdc7d22b
Kavitha Subramani <k.subramani@meridian.internal>
2023-10-23 17:58:00 +0530

    PLAT-2189 entitlements-service dual approval rules

---
+++ services/entitlements-service/src/main/java/com/meridian/platform/entitlements/role/RoleDefRepository.java
@@ -0,0 +1,6 @@
+package com.meridian.platform.entitlements.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleDefRepository extends JpaRepository<RoleDef, String> {
+}
